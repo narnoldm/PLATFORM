1a42bc6452ac80432b803a907028fc6800bba6ed
Nicholas Arnold-Medabalimi <narnoldm@umich.edu>
2020-01-12 17:05:57 -0500

    remove tecplot
    Former-commit-id: 02a2d2d92a50a977d893561dfcc2fdc40875d3a0
    Former-commit-id: ce368a02cde484a34eecdef74ebcfec5ed4107f8

--- tecio/teciosrc/TecplotVersion.h
+++
@@ -1,9 +0,0 @@
- #pragma once
- #define ___4021 2017
- #define ___4025 2
- #define ___4019 0
- #define ___4023 79771
- #define TecVersionId "2017.2.0.79771"
- #define TecplotSDKBinaryFileVersion 112
- #define TecplotAPIVersion 141
- #define TecplotAPIVersionStr "141"
