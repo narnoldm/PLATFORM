ca48c4acebe92a1c5811f4f95f0ce62d19048273
Nicholas Arnold-Medabalimi <narnoldm@umich.edu>
2020-01-12 17:05:57 -0500

    remove tecplot
    Former-commit-id: 1a42bc6452ac80432b803a907028fc6800bba6ed [formerly ce368a02cde484a34eecdef74ebcfec5ed4107f8] [formerly 02a2d2d92a50a977d893561dfcc2fdc40875d3a0]
    Former-commit-id: 25b95551f91bded01415c0deba5d460a8d54fb6a
    Former-commit-id: 639643d396f41fef1e705e729e2e7813b98210f0

--- tecio/teciosrc/FileSystem.h
+++
@@ -1,6 +0,0 @@
- #pragma once
-#include "ThirdPartyHeadersBegin.h"
-#  include <stdio.h>
-#  include <string>
-#include "ThirdPartyHeadersEnd.h"
-namespace tecplot { namespace filesystem { FILE* fileOpen(std::string const& ___1394, std::string const& ___2504); FILE* fileReopen(std::string const& ___1394, std::string const& ___2504, FILE* file); }}
