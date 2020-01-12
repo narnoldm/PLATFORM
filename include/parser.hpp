d75f97a1146d7b6efde2a8c3c0a2d14d99eae7f5
Nicholas Arnold-Medabalimi <narnoldm@umich.edu>
2020-01-12 17:04:06 -0500

    Initial commit
    Former-commit-id: ddd6e14512e5a415a63e69cb03dbc5aafe5cc431 [formerly 3c75408612bfd90d00128583f2d867e5fdf5c088] [formerly 3285e66f9afc2f00002bbb93e2d4c93c2821a506]
    Former-commit-id: b606af0877d2fc45c9986a6276de4f3ba519465b
    Former-commit-id: 44bb396a7e4455686d1eae5ff088779901780387

---
+++ include/parser.hpp
@@ -0,0 +1,186 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+#include <assert.h>
+#include <algorithm>
+#include <cctype>
+
+#include "pMat.hpp"
+
+using namespace ::std;
+
+template <class T>
+class paramID
+{
+public:
+    string key;
+    bool set;
+    T value;
+
+    paramID();
+    paramID(string kIn, T vIn);
+    paramID(string kIn);
+    ~paramID();
+
+    bool checkInfo(string line);
+};
+
+class sysInfo
+{
+public:
+    enum Debug
+    {
+        IGNORE,
+        STOP,
+        QUERY
+    };
+    paramID<long> MEMAVAIL, PC;
+    paramID<int> DEBUG;
+    sysInfo();
+    ~sysInfo();
+    bool ScanInfo(string token);
+};
+
+class matID
+{
+public:
+    enum typeenum
+    {
+        inferred,
+        input,
+        output,
+        defined
+    };
+    enum dimenum
+    {
+        mat,
+        vec
+    };
+    int type, dim;
+    vector<string> token;
+    string name;
+    int M, N;
+
+    //ParMat mat;
+
+    matID();
+    ~matID();
+    void setName(string n);
+    void setInfo(string d);
+    void setInfo(vector<int> &d);
+};
+
+class inputInfo
+{
+public:
+    enum typeenum
+    {
+        inferred,
+        input,
+        output,
+        defined
+    };
+    enum dimenum
+    {
+        scal,
+        vec,
+        mat,
+    };
+    vector<matID *> matList;
+
+    inputInfo();
+    ~inputInfo();
+
+    bool ScanInput(string token);
+    bool checkMats();
+    void assignInputInfo();
+};
+
+class operation
+{
+public:
+
+    vector<string> input;
+    vector<string> output;
+    vector<int> inID;
+    vector<int> outID;
+    vector<matID *> inputMat;
+    vector<matID *> outputMat;
+
+    string opName;
+    bool operation_ident;
+
+    operation();
+    operation(string token1, string token2);
+    ~operation();
+
+    void assignInput(string token);
+    void assignOutput(string token);
+    void checkMats(inputInfo &MatList);
+    void checkOperation(string token);
+    bool inferDim();
+    void assignOutputDim();
+};
+
+class operationQueue
+{
+public:
+    vector<operation *> taskQueue;
+
+    operationQueue();
+    ~operationQueue();
+
+    bool ScanOperations(string token);
+    void assignMats(inputInfo &MatList);
+    void inferDim();
+};
+
+class inputReader
+{
+public:
+    std::string ifile;
+    std::vector<std::string> keys;
+    std::vector<int> intParam;
+    std::vector<double> doubleParam;
+    std::vector<std::string> stringParam;
+
+    sysInfo sys;
+    inputInfo inp;
+    operationQueue op;
+    inputInfo out;
+
+    inputReader(std::string file);
+    bool ScanFile();
+};
+
+
+class executioner
+{
+    public: 
+    inputReader * inpFile;
+    //vector<dataTool*> dTs;
+    vector<PGrid*> pGs;
+    vector<pMat*> pMats;
+
+
+    executioner();
+    executioner(inputReader * iF);
+    ~executioner();
+
+    void init();
+    void exec_all();
+    void exec(int opID);
+    void clear();
+
+    void create_matricies();
+};
+
+
+
+bool to_bool(std::string str);
+
+void tokenparse(string &input, string sep, vector<string> &tokens);
---
+++ tecio/teciosrc/exportSubzonePlt.h
@@ -0,0 +1,8 @@
+ #pragma once
+#include "AltTecUtil.h"
+#include "DataSetWriter.h"
+#include "DataWriteStatisticsInterface.h"
+#include "FileDescription.h"
+#include "FileWriterInterface.h"
+#include "IJK.h"
+namespace tecplot { namespace ___3933 { ___372 ___1242( ___37&                   ___36, FileWriterInterface&          szpltFile, ___1388 const&        ___1387, ___372                     includeRevisionNumber, DataSetWriter&                dataSetWriter, ___3501                        vars, ___3501                        ___4671, ___936& ___935); ___372 exportSubzonePartitionFile( ___37&            ___36, FileWriterInterface&   szptnFile, ___1388 const& ___1387, DataSetWriter&         dataSetWriter); }}
