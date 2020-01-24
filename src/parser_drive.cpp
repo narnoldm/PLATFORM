3a78dc2b3562835ba4e79194bef340d8bb22d3f9
Nicholas Arnold-Medabalimi <narnoldm@umich.edu>
2020-01-24 10:44:40 -0500

    added test cleanup
    Former-commit-id: 5c9ced88590aea1b2af63d917981f8087d53e517 [formerly 35f4a7e5b25ae2116ca69a34de142a897bac7ccf] [formerly f19cabcab2cb4d89a9c8b156b182a57bc67b6119]
    Former-commit-id: 1d652691b4b15119ff4b683698d7f92b2f57a492
    Former-commit-id: a4794c6a6643e0b1195d94d42fe823c4d0a2e642

--- src/parser_drive.cpp
+++ src/parser_drive.cpp
@@ -11,28 +11,35 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     std::ofstream sink("/dev/null");
-    streambuf* strm_buffer = cout.rdbuf();
-    if(rank!=0)
+    streambuf *strm_buffer = cout.rdbuf();
+    if (rank != 0)
     {
         std::cout.rdbuf(sink.rdbuf());
     }
 
+
+    string input;
+    cout<<argc;
+    input="input.pdp";
+    if(argc>1)
+    {
+        string input = argv[1];
+    }
+    cout << input << endl;
     cout << "initializing" << endl;
 
-    inputReader* file1p= new inputReader("input.pdp");
+    inputReader *file1p = new inputReader("input.pdp");
     executioner *exec = new executioner(file1p);
-    
+
     exec->init();
+
     exec->exec_all();
     exec->output();
     exec->clear();
 
-
-
-    
     delete file1p;
     MPI_Barrier(MPI_COMM_WORLD);
-    cout.rdbuf (strm_buffer);
+    cout.rdbuf(strm_buffer);
     MPI_Finalize();
     return 0;
 }
