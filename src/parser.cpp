c3fe9ac516159e9896d35fd706a1368898ed52a6
Nicholas Arnold-Medabalimi <narnoldm@umich.edu>
2020-01-26 19:04:36 -0500

    Started adding operations
    Former-commit-id: 197a82dfb17e6f4adb7955936fec250509643fbb
    Former-commit-id: 3016d91afc141fb0e91feffc49fa86751bf1bb41

--- src/parser.cpp
+++ src/parser.cpp
@@ -9,7 +9,7 @@ ostream &operator<<(std::ostream &os, const paramID<T> &pID)
 }
 template <class T>
 paramID<T>::paramID()
-{ 
+{
         key = "";
         value = 0;
         set = false;
@@ -404,38 +404,60 @@ void operation::assignOutputDim()
         if (opName == "svd")
         {
                 cout << opName << " recognized" << endl;
-                if (inputMat.size() != 1 || outputMat.size() != 3)
-                {
-                        cout << "SVD should have 3 outputs and 1 input" << endl;
-                        assert(inputMat.size() != 1 || outputMat.size() != 3);
-                }
+                opInfoCheck(1,3);
                 //input contraints SVD requires mb=nb
                 inputMat[0]->compPGreq = true;
                 //output contraints
+                int minMN= min(inputMat[0]->dims[0], inputMat[0]->dims[1]);
                 vector<int> temp;
                 temp.clear();
                 temp.push_back(inputMat[0]->dims[0]);
-                temp.push_back(inputMat[0]->dims[1]);
+                temp.push_back(minMN);
                 outputMat[0]->setInfo(temp);
-                temp[0] = inputMat[0]->dims[1];
+                temp[0] = minMN;
+                temp[1] = inputMat[0]->dims[1];
                 outputMat[2]->setInfo(temp);
+
+
                 temp.clear();
-                temp.push_back(min(inputMat[0]->dims[0], inputMat[0]->dims[1]));
+                temp.push_back(minMN);
                 outputMat[1]->setInfo(temp);
                 outputMat[1]->isPA = true;
                 temp.clear();
                 return;
         }
-        
-        else
-                cout << opName << " not found" << endl;
-}
+        else if (opName == "M*M")
+        {
+                cout << opName << " recognized" << endl;
+                opInfoCheck(2,1);
+                //input contraints SVD requires mb=nb
+                inputMat[0]->compPGreq = true;
+                inputMat[1]->compPGreq = true;
+                //output contraints
+                vector<int> temp;
+                temp.clear();
+                temp.push_back(inputMat[0]->dims[0]);
+                temp.push_back(inputMat[1]->dims[1]);
+                outputMat[0]->setInfo(temp);
+                return;
+        }
+        else if (opName == "Transpose")
+        {
+                cout<<opName<<"recognized"<<endl;
+                opInfoCheck(1,1);
 
+        }
+        else
+        {
+                cout << opName << " not found" << endl;
+                throw(-1);
+        }
+}
 void operation::execute()
 {
         if (opName == "svd")
         {
-                cout << "executing SVD" << endl;
+                cout << "executing"<< opName<< endl;
                 pMat *A, *U, *S, *VT;
                 A = inputMat[0]->pMatpoint;
                 U = outputMat[0]->pMatpoint;
@@ -445,6 +467,27 @@ void operation::execute()
                 A->svd_run(A->N, A->M, 0, 0, U, VT, S->dataD);
                 cout << "SVD is destructive: Using or Outputing A is ill advised" << endl;
         }
+        else if(opName =="M*M")
+        {
+                cout << "executing "<<opName << endl;
+                pMat *C,*A,*B; // C= A*B;
+                A=inputMat[0]->pMatpoint;
+                B=inputMat[1]->pMatpoint;
+                C=outputMat[0]->pMatpoint;
+                assert(inputMat[0]->dims[1]==inputMat[1]->dims[0]);
+                C->matrix_Product('N','N',outputMat[0]->dims[0],outputMat[0]->dims[1],inputMat[0]->dims[1],inputMat[0]->pMatpoint,0,0,inputMat[1]->pMatpoint,0,0,1.0,0.0,0,0);
+        }
+
+}
+
+void operation::opInfoCheck(int in, int out)
+{
+        if (inputMat.size() != in || outputMat.size() != out)
+        {
+                cout << opName <<" should have "<<out<<" output(s) and "<<in<<" input(s)" << endl;
+                assert(inputMat.size() != in || outputMat.size() != out);
+        }
+        return;
 }
 ostream &operator<<(std::ostream &os, const operation &op)
 {
@@ -577,6 +620,7 @@ bool inputReader::ScanFile()
                         break;
                 }
         }
+        inFile.close();
         cout << sys;
         cout << inp;
         cout << op;
@@ -585,7 +629,7 @@ bool inputReader::ScanFile()
         cout << out;
 
         int i = 0;
-        while (!inp.checkMats())
+        if(!inp.checkMats())
         {
                 cout << "not all mats defined" << endl;
                 inp.assignInputInfo();
@@ -598,7 +642,13 @@ bool inputReader::ScanFile()
                         throw(-1);
                 }
         }
-        cout << inp;
+
+        if(out.matList.size()==0)
+        {
+                cout<<"No outputs defined. Add an output"<<endl;
+                throw(-1);
+        }
+
         cout << "Yay all orperations and matracies defined" << endl;
 }
 
