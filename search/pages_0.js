07b17a53a7f5ced64e6224a33f32e7dfad1b380f
narnoldm <narnoldm@users.noreply.github.com>
2022-12-19 17:09:20 +0000

    deploy: 8457d685864154368f27334d19b9d2b8d7010d8e

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['example1_0',['Example1',['../example1.html',1,'']]]
+  ['example1_0',['Example1',['../example1.html',1,'']]],
+  ['example2_1',['Example2',['../example2.html',1,'']]]
 ];
